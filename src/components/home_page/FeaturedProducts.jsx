449e42e6423e33c382969154aa7cf5cf3d112bd3
Ashik70021 <ashik70021@gmail.com>
2024-11-16 13:59:06 +0600

    implemet user revies and accordion

--- src/components/home_page/FeaturedProducts.jsx
+++ src/components/home_page/FeaturedProducts.jsx
@@ -2,11 +2,8 @@ import ProductCard from "../ProductCard";
 
 const FeaturedProducts = () => {
     return (
-        <div className="my-16 mx-24">
-            <div className="text-center text-4xl font-bold">
-                <h3 className="" >Featured Products</h3>
-            </div>
-            <div className="my-14 flex items-center justify-between gap-4">
+        <div>
+            <div className="lg:flex items-center justify-between gap-4">
                 <ProductCard></ProductCard>
                 <ProductCard></ProductCard>
                 <ProductCard></ProductCard>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,30 @@
+import Accordion from "../components/home_page/Accordion";
 import Banner from "../components/home_page/Banner";
 import FeaturedProducts from "../components/home_page/FeaturedProducts";
+import UserReview from "../components/home_page/UserReview";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <FeaturedProducts></FeaturedProducts>
+            <div className="container mx-auto">
+                <div className="my-24">
+                    <h1 className="mb-16 text-2xl font-semibold text-center" >Featured Products</h1>
+                    <FeaturedProducts></FeaturedProducts>
+                </div>
+
+                <div className="my-24">
+                    <h1 className="mb-16 text-2xl font-semibold text-center" >Reviews</h1>
+                    <UserReview></UserReview>
+                </div>
+
+                <div className="my-24">
+                    <h1 className="mb-16 text-2xl font-semibold text-center" >Frequently asked questions</h1>
+                    <Accordion></Accordion>
+                </div>
+
+            </div>
+
         </div>
     );
 };
