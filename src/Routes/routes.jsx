9ab3ff97ad406249a07f137086ca6bd914db7ff7
Ashik70021 <ashik70021@gmail.com>
2024-11-16 20:47:27 +0600

    implement login and register

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,6 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 import Home from "../pages/Home";
+import Products from "../pages/Products";
+import AboutUs from "../pages/AboutUs";
+import ContactUs from "../pages/ContactUs";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 export const router = createBrowserRouter([
     {
@@ -10,7 +15,27 @@ export const router = createBrowserRouter([
         {
           path: "/",
           element: <Home></Home>,
-        }
+        },
+        {
+          path: "/products",
+          element: <Products></Products>,
+        },
+        {
+          path: "/about",
+          element: <AboutUs></AboutUs>,
+        },
+        {
+          path: "/contact",
+          element: <ContactUs></ContactUs>,
+        },
+        {
+          path: "/login",
+          element: <Login></Login>,
+        },
+        {
+          path: "/register",
+          element: <Register></Register>,
+        },
       ]
     },
   ]);
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <div className="navbar bg-base-100">
@@ -20,36 +22,27 @@ const Navbar = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        <li> <NavLink to='/'>Home</NavLink> </li>
+                        <li> <NavLink to='products'>Products</NavLink> </li>
+                        <li> <NavLink to='about'>About us</NavLink> </li>
+                        <li> <NavLink to='contact'>Contact us</NavLink> </li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">Gadget Shop</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    <li> <NavLink to='/'>Home</NavLink> </li>
+                    <li> <NavLink to='products'>Products</NavLink> </li>
+                    <li> <NavLink to='about'>About us</NavLink> </li>
+                    <li> <NavLink to='contact'>Contact us</NavLink> </li>
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <div className="flex gap-2">
+                    <Link to='/login'><button className="btn btn-outline btn-primary text-white rounded-md">Login</button></Link>
+                    <Link to='/register'><button className="btn btn-active btn-primary text-white rounded-md">Register</button></Link>
+                </div>
             </div>
         </div>
     );
